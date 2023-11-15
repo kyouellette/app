688567359b77cad6317f311531c92b18ec29917b
PLKyle <kyle@playerslounge.co>
2023-11-15 15:35:22 -0500

    new app splash

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
-import React, { useCallback } from 'react';
-import { StyleSheet, Text, View, StatusBar, LogBox } from 'react-native';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+import { StyleSheet, Text, View, StatusBar, LogBox, Animated, Easing } from 'react-native';
 import { Colors } from './src/style/colors';
 import { Router } from './src/navigation/stack-navigator'; // Import your StackNavigator
-// import * as SplashScreen from 'expo-splash-screen';
+import LottieSplash from './assets/animated-splash.json';
+import LottieView from 'lottie-react-native';
 import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context';
 import { AuthProvider } from './src/contexts/auth-context';
 
@@ -22,27 +24,61 @@ const CustomStatusBar = ({
   )
 }
 
-export default function App() {
-  LogBox.ignoreAllLogs();
-  // May use later, load custom font
-  // const [fontsLoaded] = useFonts({
-  //   'Semi-Bold': require('./assets/fonts/Semi-Bold.ttf'),
-  // });
-  // const onLayoutRootView = useCallback(async () => {
-  //   if (fontsLoaded) {
-  //     await SplashScreen.hideAsync();
-  //   }
-  // }, [fontsLoaded]);
+SplashScreen.preventAutoHideAsync();
 
-  // if (!fontsLoaded) {
-  //   return null;
-  // }
+export default function App() {
+  const [isAppReady, setAppReady] = useState<boolean>(false);
+  const [isLayoutReady, setLayoutReady] = useState<boolean>(false);
+  const animationProgress = useRef(new Animated.Value(0));
+  LogBox.ignoreAllLogs();
+
+  useEffect(() => {
+       const animation = Animated.timing(animationProgress.current, {
+            toValue: 1,
+            duration: 3000,
+            easing: Easing.linear,
+            useNativeDriver: true,
+        })
+        animation.start(() => {
+          setAppReady(true);
+        });
+}, []);
+
+const onApplicationReady = () => {
+  setAppReady(true);
+};
+
+const onLayout = useCallback(async () => {
+  try {
+      await SplashScreen.hideAsync();
+  } catch (e) {
+  } finally {
+      setLayoutReady(true);
+  }
+}, []);
+
+const showAnimation = !(isAppReady && isLayoutReady);
 
   return (
     <AuthProvider>
     <SafeAreaProvider style={{backgroundColor: Colors.blackOne}}>
+    {isAppReady && (
+      <>
       <CustomStatusBar backgroundColor={Colors.blackOne}/>
       <Router />
+      </>
+        )}
+          {showAnimation && (
+          <View
+            pointerEvents="none"
+            style={{flex: 1}}
+            onLayout={onLayout}>
+              <LottieView
+                source={LottieSplash}
+                loop={false}
+                progress={animationProgress.current}/>
+          </View>
+            )}
     </SafeAreaProvider>
     </AuthProvider>
   );
